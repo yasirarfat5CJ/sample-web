fb17f6e1cfbb8ac9c28427429bb2fa1016bb16c4
yasir arfat <yasir.arfat.cse@gmail.com>
2025-07-03 14:50:41 +0530

    msg

--- src/components/Gallery.jsx
+++ src/components/Gallery.jsx
@@ -9,33 +9,30 @@ function Gallery() {
         {/* Sample Images */}
         <img src="/images/img1.jpg" alt="Gallery Pic 1" />
         <img src="/images/img2.jpg" alt="Gallery Pic 2" />
-        <img src="/images/img3.jpg" alt="Gallery Pic 3" />
-        <img src="/images/img4.jpg" alt="Gallery Pic 3" />
+     
+       
 <img src="/images/img5.jpg" alt="Gallery Pic 3" />
-<img src="/images/img6.jpg" alt="Gallery Pic 3" />
+
 <img src="/images/img7.jpg" alt="Gallery Pic 3" />
 <img src="/images/img8.jpg" alt="Gallery Pic 3" />
 
 <img src="/images/img19.jpg" alt="Gallery Pic 3" />
 <img src="/images/img20.jpg" alt="Gallery Pic 3" />
 <img src="/images/img10.jpeg" alt="Gallery Pic 3" />
-<img src="/images/img11.jpeg" alt="Gallery Pic 3" />
+
 <img src="/images/img12.jpeg" alt="Gallery Pic 3" />
 <img src="/images/img21.jpg" alt="Gallery Pic 3" />
 <img src="/images/img13.jpeg" alt="Gallery Pic 3" />
-<img src="/images/img14.jpeg" alt="Gallery Pic 3" />
+
 <img src="/images/img15.jpeg" alt="Gallery Pic 3" />
-<img src="/images/img16.jpeg" alt="Gallery Pic 3" />
+
 <img src="/images/img17.jpeg" alt="Gallery Pic 3" />
-<img src="/images/img18.jpeg" alt="Gallery Pic 3" />
+
 
 
 
         {/* Sample Video */}
-        <video controls>
-          <source src="/images/video1.mp4" type="video/mp4" />
-          Your browser does not support the video tag.
-        </video>
+       
         <video controls>
           <source src="/images/video2.mp4" type="video/mp4" />
           Your browser does not support the video tag.
